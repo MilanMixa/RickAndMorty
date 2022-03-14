91c9b3647918d0933ae42e978b0a727a7895a1da
MilanMixa <stankovicmilan990@gmail.com>
2022-03-14 16:26:03 +0100

    adding a bit of styling to the SingleCharacerRoute component

--- src/containers/SingleCharacterRoute/SingleCharacterRoute.jsx
+++ src/containers/SingleCharacterRoute/SingleCharacterRoute.jsx
@@ -6,11 +6,47 @@ export default function SingleCharacterRoute() {
   const { id } = useParams();
   const { singleCharacter, singleCharacterStatus } =
     useSingleCharacterRoute(id);
+  console.log(singleCharacter);
+
+  // name, status, species, origin.name, location.name, image
 
   return (
     <AsyncList status={singleCharacterStatus}>
       {/* <div className="text-white">{singleCharacter && singleCharacter?.name}</div> */}
-      <div className="text-white">{singleCharacter?.name}</div>
+
+      {/* <div>{singleCharacter?.name}</div>
+        <div>{singleCharacter?.status}</div>
+        <div>{singleCharacter?.species}</div>
+        <div>{singleCharacter?.origin.name}</div>
+        <div>{singleCharacter?.location.name}</div>
+        <img src={singleCharacter?.image} alt="image" /> */}
+      <div className="text-white bg-yellow-500 flex">
+        <div className="w-1/3">
+          <img
+            className="rounded-2xl"
+            src={singleCharacter?.image}
+            alt="avatar"
+          />
+          <h3 className="bg-gray-500 rounded-xl py-3 text-center">
+            Name: {singleCharacter?.name}
+          </h3>
+        </div>
+        {/* mozda mora u jedan parent div */}
+        <div className="">
+          <div className="bg-gray-500 rounded-xl py-3 text-center my-2">
+            <p>Status: {singleCharacter?.status}</p>
+          </div>
+          <div className="bg-gray-500 rounded-xl py-3 text-center">
+            <p>Species: {singleCharacter?.species}</p>
+          </div>
+          <div className="bg-gray-500 rounded-xl py-3 text-center my-2">
+            <p>Origin: {singleCharacter?.origin.name}</p>
+          </div>
+          <div className="bg-gray-500 rounded-xl py-3 text-center">
+            <p>Location: {singleCharacter?.location.name}</p>
+          </div>
+        </div>
+      </div>
     </AsyncList>
   );
 }
